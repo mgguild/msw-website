c9a27b3e11a0ccb2e54605e11fd5d2f8f7bc3c2e
vkth <ghieianagee@gmail.com>
2022-04-08 17:37:38 +0800

    Video intro background link change

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
-/*!
-    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
-    */
     (function($) {
-    "use strict"; // Start of use strict
+    //"use strict"; // Start of use strict
   
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
@@ -65,5 +60,6 @@
       });
     });
   
+    
   })(jQuery); // End of use strict
   
