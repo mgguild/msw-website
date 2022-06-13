d49957a506f13b0bec52aa8c660be117745fca69
vkth <ghieianagee@gmail.com>
2022-06-13 11:27:59 +0800

    Partial updates

--- js/scripts.js
+++ js/scripts.js
@@ -134,6 +134,22 @@
           $("#video").attr('src', ''); 
       }) 
       });
+
+      document.querySelector(".card-flip").classList.toggle("flip");
+
+      /* 
+      * Holder.js for demo image
+      * Just for demo purpose
+      */
+      Holder.addTheme('gray', {
+        bg: '#777',
+        fg: 'rgba(255,255,255,.75)',
+        font: 'Helvetica',
+        fontweight: 'normal'
+      });
+
+
+      
     
   })(jQuery); // End of use strict
   
