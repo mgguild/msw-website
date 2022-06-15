5d7e764beb3a06bc5128b35eab80e4d68dc56b69
vkth <ghieianagee@gmail.com>
2022-06-16 00:07:55 +0800

    Adjust placement of pop-up modal embeded youtube link to center resolves issue #76

--- js/scripts.js
+++ js/scripts.js
@@ -126,7 +126,7 @@
       // autoplay video on modal load  
       $('#myModal').on('shown.bs.modal', function (e) {
       // youtube iframe configuration and settings
-      $("#video").attr('src',$videoSrc ); 
+      $("#video").attr('src',$videoSrc + "?autoplay=1&rel=0&controls=1&loop=1&modestbranding=1" ); 
       })
       // stop playing the youtube video when modal closed
       $('#myModal').on('hide.bs.modal', function (e) {
