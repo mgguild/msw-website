f1035d63ad47b649580bfd7a13d44f99302ce60b
vkth <42258463+vkth@users.noreply.github.com>
2022-11-21 18:35:01 +0800

    Fix menu buttons in mobile view resolves issue #115

--- js/scripts.js
+++ js/scripts.js
@@ -62,14 +62,6 @@
      var $spy = $(this).scrollspy('refresh')
    })
   
-  // // Collapse Navbar
-  // var navbarCollapse = function() {
-  //   if ($("#mainNav").offset().top > 100) {
-  //     $("#mainNav").addClass("navbar-shrink");
-  //   } else {
-  //     $("#mainNav").removeClass("navbar-shrink");
-  //   }
-  // };
 
    // Collapse now if page is not at top
    navbarCollapse();
