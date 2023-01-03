f2113d36e89fce9944f0a5756063f91d08d2d4ca
vkth <ghieianagee@gmail.com>
2023-01-03 17:31:29 +0800

    Add revelead one of ones resolves  issue #134

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 71)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
+  //Smooth scrolling
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
   });
+
+
+  // Get the container element
+  var btnContainer = document.getElementById("mainNav");
+
+  // Get all buttons with class="btn" inside the container
+  var btns = btnContainer.getElementsByClassName("nav-link");
+
+  // Loop through the buttons and add the active class to the current/clicked button
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
 
   // Scroll to top button appear
   $(document).scroll(function() {
@@ -30,15 +49,16 @@
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
+  
+  // // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#mainNav',
-    offset: 90
-  });
+     target: '#mainNav',
+     offset: 80
+   });
 
-  $('[data-spy="scroll"]').each(function () {
-    var $spy = $(this).scrollspy('refresh')
-  })
+   $('[data-spy="scroll"]').each(function () {
+     var $spy = $(this).scrollspy('refresh')
+   })
   
   // Collapse Navbar
   var navbarCollapse = function() {
@@ -54,64 +74,8 @@
    // Collapse the navbar when page is scrolled
    $(window).scroll(navbarCollapse);
  
-  
-  
-    function moveToSelected(element) {
-
-      if (element == "next") {
-        var selected = $(".selected").next();
-      } else if (element == "prev") {
-        var selected = $(".selected").prev();
-      } else {
-        var selected = element;
-      }
     
-      var next = $(selected).next();
-      var prev = $(selected).prev();
-      var prevSecond = $(prev).prev();
-      var nextSecond = $(next).next();
-    
-      $(selected).removeClass().addClass("selected");
-    
-      $(prev).removeClass().addClass("prev");
-      $(next).removeClass().addClass("next");
-    
-      $(nextSecond).removeClass().addClass("nextRightSecond");
-      $(prevSecond).removeClass().addClass("prevLeftSecond");
-    
-      $(nextSecond).nextAll().removeClass().addClass('hideRight');
-      $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-    
-    }
-    
-
-    $(document).keydown(function(e) {
-        switch(e.which) {
-            case 37: // left
-            moveToSelected('prev');
-            break;
-    
-            case 39: // right
-            moveToSelected('next');
-            break;
-    
-            default: return;
-        }
-        e.preventDefault();
-    });
-    
-    $('#carousel div').click(function() {
-      moveToSelected($(this));
-    });
-    
-    $('#prev').click(function() {
-      moveToSelected('prev');
-    });
-    
-    $('#next').click(function() {
-      moveToSelected('next');
-    });
-    
+    // FOR YT BUTTON
     $(document).ready(function() {
       // get video source from data-src button
       var $videoSrc;  
@@ -131,10 +95,12 @@
       }) 
       });
 
+      //for NFT cards
       document.querySelector(".card-flip").classList.toggle("flip");
 
       document.querySelector(".mobile-card-flip").classList.toggle("mobile-flip");
 
+      //for FAQs
       $(".open").click( function () {
         var container = $(this).parents(".topic");
         var answer = container.find(".answer");
@@ -156,6 +122,8 @@
           container.addClass("expanded");
         }
       });
-
+      
+    
+      
   })(jQuery); // End of use strict
   
