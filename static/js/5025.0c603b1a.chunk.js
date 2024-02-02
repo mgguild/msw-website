9f97b8fcbd06fe1bfd5ad7a457d663ed92f66910
swengr-janan <swengr-janan@users.noreply.github.com>
2024-02-02 10:33:22 +0000

    Deploying to gh-pages from @ mgguild/msw-website@226e4f74e076212b243317878fa4f284bd79dbf3 🚀

---
+++ static/js/5025.0c603b1a.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkMetaSaga_Warriors=self.webpackChunkMetaSaga_Warriors||[]).push([[5025],{25025:e=>{e.exports=JSON.parse('[{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]')}}]);
