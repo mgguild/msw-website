1615c21a05bc1f4a0d7b3871ec44dc7e60fa9d48
Salvador Amba Jr <46850774+Buko-pie@users.noreply.github.com>
2024-04-01 06:17:33 +0800

    Added scree size condition to text, images, and div sizes
    Change marketplace navbar panel to outside of bodywrapp in order to be sticky
    
    changed styled media queries porprties in body wrapp

--- src/Components/Marketplace/views/MarketplaceV2/components/Navbar/index.tsx
+++ src/Components/Marketplace/views/MarketplaceV2/components/Navbar/index.tsx
@@ -1,19 +1,38 @@
-import { LoginRegister, UserDashboard } from '../../../../../Modals';
+import { useState, useMemo } from 'react';
 import styled from 'styled-components';
-import useTheme from '../../../../hooks/useTheme';
+import { LoginRegister, UserDashboard } from '../../../../../Modals';
 import useMarketplaceV2 from '../../../../hooks/useMarketplaceV2';
 import { HEIGHT, PADDING } from '../../styles/constants';
 import { mswURL } from '../../constants/config';
 import Logo from '../Foundation/Logo';
-import { useAppDispatch } from '../../../../state';
 import usePlayfab from '../../../../../../Hooks/usePlayfab';
+
+const StyledNav = styled.nav<{ isScreen480?: boolean }>`
+    background-color: #181020;
+    display: flex;
+    flex-flow: ${({isScreen480}) => (isScreen480 ? 'column wrap' : 'row wrap')};
+    justify-content: space-between;
+    align-items: center;
+    justify-content: ${({isScreen480}) => (isScreen480 ? 'center' : 'none')};;
+    padding: 1em 5em 1em 5em !important;
+`;
 
 const Navbar = () => {
     const { controllers } = useMarketplaceV2();
     const user = usePlayfab((state: any) => state.user);
 
+    const [isScreen480, setIsScreen480] = useState(false);
+
+    const handleResize = () => {
+        setIsScreen480(window.innerWidth < 480);
+    };
+
+    useMemo(() => {
+        handleResize();
+    }, []);
+
     return (
-        <StyledNav>
+        <StyledNav isScreen480={isScreen480}>
             <Logo size={169} url={mswURL} />
             <div>{user ? <UserDashboard /> : <LoginRegister />}</div>
         </StyledNav>
@@ -21,13 +40,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-const StyledNav = styled.nav`
-    background-color: #181020;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1em 5em 1em 5em !important;
-`;
 
 const StyledBtn = styled.button``;
